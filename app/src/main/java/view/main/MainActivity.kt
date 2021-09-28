e74d34087b58330d314aa2b56f9711c22e6f8dfb
Tyurins <turin.92@mail.ru>
2021-09-28 22:19:10 +0400

    homework 9 in process

--- app/src/main/java/view/main/MainActivity.kt
+++ app/src/main/java/view/main/MainActivity.kt
@@ -1,14 +1,43 @@
 package view.main
 
+import android.Manifest
+import android.database.Cursor
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
 import com.example.myweatherapp.R
 import com.example.myweatherapp.databinding.MainActivityBinding
 import view.details.HistoryFragment
 
+
 class MainActivity : AppCompatActivity() {
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    private val permissionResult =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { result ->
+            when {
+                result -> getContact()
+                !shouldShowRequestPermissionRationale(Manifest.permission.READ_CONTACTS) ->
+                    Toast.makeText(
+                        this,
+                        "Увы вы не дали разрешения к контактам :-(",
+                        Toast.LENGTH_LONG
+                    ).show()
+
+                else -> Toast.makeText(
+                    this,
+                    "Увы вы не дали разрешения к контактам :-(",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
 
     private lateinit var binding: MainActivityBinding
 
@@ -23,6 +52,7 @@ class MainActivity : AppCompatActivity() {
                 .commitAllowingStateLoss()
         }
     }
+
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_history, menu)
         return super.onCreateOptionsMenu(menu)
@@ -39,7 +69,40 @@ class MainActivity : AppCompatActivity() {
                 }
                 true
             }
+            R.id.menu_content_provider -> {
+                permissionResult.launch(Manifest.permission.READ_CONTACTS)
+                true
+            }
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    private fun getContact() {
+        contentResolver
+
+        val cursor: Cursor? = contentResolver.query(
+            ContactsContract.Contacts.CONTENT_URI,
+            null,
+            null,
+            null,
+            ContactsContract.Contacts.DISPLAY_NAME + "ASC"
+        )
+
+        val contracts = mutableListOf<String>()
+        cursor?.let {
+            for (i in 0..cursor.count) {
+                if (cursor.moveToPosition(i)) {
+                    val name =
+                        cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME))
+
+                    contracts.add(name)
+                }
+            }
+        }
+
+        AlertDialog.Builder(this)
+            .setItems(contracts.toTypedArray()) { d, w -> }
+            .setCancelable(true)
+            .show()
     }
 }
